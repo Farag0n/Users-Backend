dc405326796ec0250a906896201b86d5d4b780fa
faragon <miguelanga1604@gmail.com>
2026-01-15 11:16:32 -0500

    hotfix

--- Users-Backend.Application/DTOs/UserLoginDto.cs
+++ Users-Backend.Application/DTOs/UserLoginDto.cs
@@ -2,6 +2,7 @@ namespace Users_Backend.Application.DTOs;
 
 public class UserLoginDto
 {
+    //TODO usar data notations
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
